2fde548abeaea0fe7b65cf02bbe8da5a309a2d4c
samuel <typewhatrecord1@gmail.com>
2026-01-29 13:11:29 +0800

    single spelling mistake made me waste like 20mins of my life... lmao

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ var closeButton = document.querySelector(".close-button");
 
 function toggleModal(id) {
     var modal = document.getElementById(id);
-    modal.style.visiblity = 'visible';
-    console.log(modal)
-    console.log(modal.style.visibility)
+    modal.style.visibility = 'visible';
+    console.log(modal);
+    console.log(modal.style.visibility);
 }
 
 closeButton.addEventListener("click", toggleModal);
