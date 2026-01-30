d2ee350ffd52e0191df3952045bd854d321a0d7b
samuel <typewhatrecord1@gmail.com>
2026-01-30 08:49:55 +0800

    finally figured out the issue that was plaguing me in my dreams (fixed modals not closing - apparently smth to do with the redundant close button code even though I didn't even use it?)

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-var closeButton = document.querySelector(".close-button");
-
-function toggleModal(id) {
-    var modal = document.getElementById(id);
-    modal.style.visibility = 'visible';
-    console.log(modal);
-    console.log(modal.style.visibility);
+var modal;
+function openModal(id) {
+    modal = document.getElementById(id);
+    toggleModal();
 }
 
-closeButton.addEventListener("click", toggleModal);
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+    console.log("switch");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal && modal.classList.contains("show-modal") == true) {
+        toggleModal();
+    }
+}
+
+window.addEventListener("click", windowOnClick);
